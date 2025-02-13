c20a06ae071364405542a5a8e8413cbec15e9ffe
Adrian Tudev <adrianotgonzaya07@gmail.com>
2025-02-13 20:46:05 +0100

    implemented book related api functions

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
+from routes import books
+
 from app.core.config import settings
 
 app = FastAPI(
   title="ChalmerShelf",        # settings.PROJECT_NAME
 )
+
+app.include_router(books.router)
---
+++ backend/app/routes/books.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+import schemas, crud, database
+
+router = APIRouter()
+
+@router.post("/books/", response_model=schemas.Book)
+def create_book(book: schemas.BookCreate, db: Session = Depends(database.get_db), seller_id: int = 1):
+    return crud.create_book(db, book, seller_id)
+
+@router.get("/books/", response_model=list[schemas.Book])
+def get_books(db: Session = Depends(database.get_db), skip: int = 0, limit: int = 10):
+    return crud.get_books(db, skip, limit)
+
+@router.get("/books/{course_code}", response_model=list[schemas.Book])
+def get_books_by_course(course_code: str, db: Session = Depends(database.get_db)):
+    books = crud.get_books_by_course(db, course_code)
+    if not books:
+        raise HTTPException(status_code=404, detail="No books found")
+    return books
