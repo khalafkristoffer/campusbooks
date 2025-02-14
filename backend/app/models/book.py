6f63c23494e7f531cfa7561103912f2f234bea00
Kristoffer <khalafkristoffer@gmail.com>
2025-02-14 11:20:36 +0100

    typo

--- backend/app/models/book.py
+++ backend/app/models/book.py
@@ -6,9 +6,9 @@ class BookDBModel(Base):
     __tablename__ = "books"
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String(100), index=True)
-    aufothor = Column(String(100))
+    author = Column(String(100))
     course_code = Column(String, ForeignKey("courses.code"))
-    description = Column(String(500)) #temp delete if issues
+    description = Column(String(500))
     condition = Column(String(50))
     price = Column(Integer)
     location = Column(String(100))
